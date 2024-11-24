91f2de953b9308d3f5c7d5104016cff7f2bafcb6
Viktor <160116173+biliavskyi03@users.noreply.github.com>
2024-11-24 15:13:58 +0200

    add db setup

--- back/src/common/config.ts
+++ back/src/common/config.ts
@@ -4,6 +4,7 @@ dotenv.config()
 const config = {
   port: parseInt(process.env.PORT || "3000", 10),
   db: process.env.MONGO_URL || "",
+  secret: process.env.SECRET || 'secret'
 }
 
 export default config
--- back/src/helpers/index.ts
+++ back/src/helpers/index.ts
@@ -1,10 +1,9 @@
+import config from "../common/config"
 import { createHmac, randomBytes } from "crypto"
-
-const SECRET = process.env.SECRET
 
 export const random = () => randomBytes(128).toString("base64")
 export const auth = (salt: string, password: string) => {
   return createHmac("sha256", [salt, password].join("/"))
-    .update(SECRET)
+    .update(config.secret)
     .digest("hex")
 }
