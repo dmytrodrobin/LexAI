ebd70dc37b3b4bc3c6311a25e952c8212aa518db
Viktor <160116173+biliavskyi03@users.noreply.github.com>
2024-11-24 13:29:34 +0200

    use auth router in global router

--- back/src/router/index.ts
+++ back/src/router/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import auth from './auth';
 
 const router = express.Router();
 
 export default (): express.Router => {
     router.get("/hello", (req: express.Request, res: express.Response) => {res.status(200).send('<p>Hello World!</p>')})
+    auth(router)
     return router;
 }
