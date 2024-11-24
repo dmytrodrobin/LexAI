e401a9463e99053f88356d765cf229b96820bebf
Viktor <160116173+biliavskyi03@users.noreply.github.com>
2024-11-24 13:49:59 +0200

    fix middleware return type

--- back/src/middleware/index.ts
+++ back/src/middleware/index.ts
@@ -1,30 +1,32 @@
-import { getUserByToken } from "repositories/user";
-import express from "express";
-import { merge } from "lodash";
-import { Constants } from "common/constants";
+import { getUserByToken } from "repositories/user"
+import express from "express"
+import { merge } from "lodash"
+import { Constants } from "common/constants"
 export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ) => {
   try {
-    const data = JSON.parse(req.cookies[Constants.AuthTokenName]);
+    const data = JSON.parse(req.cookies[Constants.AuthTokenName])
 
     if (!data.sessionToken) {
-      return res.sendStatus(403);
+      res.sendStatus(403)
+      return
     }
 
-    const user = await getUserByToken(data.sessionToken);
+    const user = await getUserByToken(data.sessionToken)
 
     if (!user) {
-      return res.sendStatus(403);
+      res.sendStatus(403)
+      return
     }
 
-    merge(req, { identity: user });
+    merge(req, { identity: user })
 
-    return next();
+    next()
   } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
+    console.log(error)
+    res.sendStatus(400)
   }
-};
+}
