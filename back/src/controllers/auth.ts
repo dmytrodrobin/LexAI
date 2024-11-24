89281af8db4de8b8958916106ccbf1b0f9f8f070
Viktor <160116173+biliavskyi03@users.noreply.github.com>
2024-11-24 13:14:40 +0200

    add auth controller

---
+++ back/src/controllers/auth.ts
@@ -0,0 +1,70 @@
+import express from "express";
+import { createUser, getUserByEmail } from "repositories/user";
+import { random, auth } from "../helpers";
+import { Constants } from "common/constants";
+
+export async function login(req: express.Request, res: express.Response) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+auth.salt +auth.password"
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = auth(user.auth.salt, password);
+
+    if (user.auth.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.auth.sessionToken = auth(salt, user._id.toString());
+
+    await user.save();
+    const cookieData = { sessionToken: user.auth.sessionToken };
+    res.cookie(Constants.AuthTokenName, JSON.stringify(cookieData), { path: "/" });
+
+    return res.status(200).json(user).end();
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(400);
+  }
+}
+export async function register(req: express.Request, res: express.Response) {
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email);
+
+    if (user) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+    const newUser = await createUser({
+      email,
+      username,
+      auth: {
+        salt,
+        password: auth(salt, password),
+      },
+    });
+
+    return res.status(200).json(newUser).end();
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(400);
+  }
+}
---
+++ back/src/helpers/index.ts
@@ -0,0 +1,10 @@
+import { createHmac, randomBytes } from "crypto";
+
+const SECRET = process.env.SECRET;
+
+export const random = () => randomBytes(128).toString("base64");
+export const auth = (salt: string, password: string) => {
+  return createHmac("sha256", [salt, password].join("/"))
+    .update(SECRET)
+    .digest("hex");
+};
