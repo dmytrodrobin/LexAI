04c7f3087510c297e8ab391c59ad11ee08918d6d
Viktor <160116173+biliavskyi03@users.noreply.github.com>
2024-11-25 15:24:38 +0200

    add `Dockerfile`, update `docker-compose`

--- back/src/common/config.ts
+++ back/src/common/config.ts
@@ -3,6 +3,7 @@ dotenv.config()
 
 const config = {
   port: parseInt(process.env.PORT || "3000", 10),
+  host: process.env.PORT || 'localhost',
   db: process.env.MONGO_URL || "",
   secret: process.env.SECRET || "secret",
   rag: process.env.RAG_URL || "",
